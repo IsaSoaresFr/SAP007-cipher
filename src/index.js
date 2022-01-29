2294a915168a94ec4b3740794216e3404801b845
IsaSoaresFr <87054089+IsaSoaresFr@users.noreply.github.com>
2022-01-28 23:33:25 -0300

    arrumei um input que estava errado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import cipher from './cipher.js';
 
-function cifrar() {
- addEventListener
- var valor = document.querySelector('#Cifrar').value;
+const botao = document.getElementById("botaoCifrar")
+botao.addEventListener("click", mostrarRespostas)
 
-document.querySelector('#Decifrar').innerHTML = valor;
+
+function mostrarRespostas(){
+    window.write("testes" #Decifrar)
+    return 
 }
+
 
 
 
