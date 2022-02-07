6c7d0bb230acef99d9f2516a450237cb6484adc7
IsaSoaresFr <87054089+IsaSoaresFr@users.noreply.github.com>
2022-02-07 19:27:22 -0300

    Adicionei um botão para recarregar a página

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import cipher from './cipher.js';
 
+const btnReset = document.querySelector('#refresh')
+
+btnReset.addEventListener("click", () =>{
+ location.reload()
+})
+
 document.getElementById("botaoCifrar").addEventListener("click", textEncrypt);
 
 function textEncrypt () {
 
-let textValue = document.getElementById("cifrarDecifrar").value;
+let textValue = document.getElementById("cifrarDecifrar").value.toUpperCase();
 let oFfset = document.getElementById("offset").value;
 let converter = Number(oFfset);
 let resposta = document.getElementById("resultEncDec");
@@ -19,7 +25,7 @@ document.getElementById("botaoDecifrar").addEventListener("click",textDecrypt );
 
 function textDecrypt () {
 
-let textValue = document.getElementById("cifrarDecifrar").value;
+let textValue = document.getElementById("cifrarDecifrar").value.toUpperCase();
 let oFfset = document.getElementById("offset").value;
 let converter = Number(oFfset);
 let resposta = document.getElementById("resultEncDec");
