2a250fa5e0b2c3a4c73028e9c383e0f5d3f1419c
IsaSoaresFr <87054089+IsaSoaresFr@users.noreply.github.com>
2022-02-15 15:18:16 -0300

    alguns ajustes

--- src/cipher.js
+++ src/cipher.js
@@ -1,55 +1,58 @@
 const cipher = { //essa constante é um objeto, irá conter toda a função de Cifrar  Decifrar, será chamada no index.
 
   encode: function (offset, string) {
-    if( typeof offset !== "number" || typeof string !== "string") {
-      throw new TypeError }
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError
+    }
 
-    let resultadoEncode = " "
+    let resultadoEncode = ""
     let quantidadeDeLetras = string.length
-    
-    for(let i=0; i<quantidadeDeLetras; i++) {
-    let valorTabelaAscii = string.charCodeAt(i)
-    let codPrimeiraLetra = 65
-   
-    if (valorTabelaAscii >= 32 && valorTabelaAscii <= 122 ) {
-    let valorCodificado = ((valorTabelaAscii - codPrimeiraLetra + offset)% 26) + codPrimeiraLetra;
-    resultadoEncode = resultadoEncode.concat(String.fromCharCode(valorCodificado))
 
-   } else {
-    let valorCodificado = ((valorTabelaAscii - codPrimeiraLetra + offset)% 26) + codPrimeiraLetra;
-    resultadoEncode = resultadoEncode.concat(String.fromCharCode(valorCodificado))
-   }
-   }
-  return resultadoEncode;
-   },
+    for (let i = 0; i < quantidadeDeLetras; i++) {
+      let valorTabelaAscii = string.charCodeAt(i)
+      let codPrimeiraLetra = 65
+      let codMinuscula = 97
+
+      if (valorTabelaAscii >= 65 && valorTabelaAscii <= 90) {
+        let valorCodificado = ((valorTabelaAscii - codPrimeiraLetra + offset) % 26) + codPrimeiraLetra;
+        resultadoEncode = resultadoEncode.concat(String.fromCharCode(valorCodificado))
+
+      } else if (valorTabelaAscii >= 97 && valorTabelaAscii <= 122) {
+        let valorCodificado = ((valorTabelaAscii - codMinuscula + offset) % 26) + codMinuscula;
+        resultadoEncode = resultadoEncode.concat(String.fromCharCode(valorCodificado))
+      }
+    }
+    return resultadoEncode;
+  },
 
 
 
 
   decode: function (offset, stringg) {
-    if( typeof offset !== "number" || typeof stringg !== "string") {
-      throw new TypeError }
+    if (typeof offset !== "number" || typeof stringg !== "string") {
+      throw new TypeError
+    }
 
-    let resultadoDecode = " "
+    let resultadoDecode = ""
     let quantidadeDeLetras2 = stringg.length
 
-    for(let i=0; i<quantidadeDeLetras2; i++){
-    let valorTabelaAscii2 = stringg.charCodeAt(i)
-    let codUltimaLetra = 90
+    for (let i = 0; i < quantidadeDeLetras2; i++) {
+      let valorTabelaAscii2 = stringg.charCodeAt(i)
+      let codUltimaLetra = 90
 
-     if (valorTabelaAscii2 >= 32 && valorTabelaAscii2 <= 122 ) {
-    let valorDecodificado = ((valorTabelaAscii2 - codUltimaLetra - offset)% 26) + codUltimaLetra;
-    resultadoDecode = resultadoDecode.concat(String.fromCharCode(valorDecodificado))
+      if (valorTabelaAscii2 >= 32 && valorTabelaAscii2 <= 122) {
+        let valorDecodificado = ((valorTabelaAscii2 - codUltimaLetra - offset) % 26) + codUltimaLetra;
+        resultadoDecode = resultadoDecode.concat(String.fromCharCode(valorDecodificado))
+      }
+      else {
+        let valorDecodificado = ((valorTabelaAscii2 - codUltimaLetra - offset) % 26) + codUltimaLetra;
+        resultadoDecode = resultadoDecode.concat(String.fromCharCode(valorDecodificado))
+      }
+    }
+    return resultadoDecode;
+  }
 }
-   else { 
-   let valorDecodificado = ((valorTabelaAscii2 - codUltimaLetra - offset)% 26) + codUltimaLetra;
-   resultadoDecode = resultadoDecode.concat(String.fromCharCode(valorDecodificado))
-   }
-   }
-  return resultadoDecode;
-   }
-   }
-   
-   
 
-  export default cipher;
+
+
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,37 +2,37 @@ import cipher from './cipher.js';
 
 const btnReset = document.querySelector('#refresh')
 
-btnReset.addEventListener("click", () =>{
- location.reload()
+btnReset.addEventListener("click", () => {
+    location.reload()
 })
 
 
 document.getElementById("botaoCifrar").addEventListener("click", textEncrypt);
 
-function textEncrypt () {
+function textEncrypt() {
 
-let textValue = document.getElementById("cifrarDecifrar").value.toUpperCase();
-let oFfset = document.getElementById("offset").value;
-let converter = Number(oFfset);
-let resposta = document.getElementById("resultEncDec");
-let valorCod = cipher.encode(converter, textValue);
+    let textValue = document.getElementById("cifrarDecifrar").value.toUpperCase();
+    let oFfset = document.getElementById("offset").value;
+    let converter = Number(oFfset);
+    let resposta = document.getElementById("resultEncDec");
+    let valorCod = cipher.encode(converter, textValue);
 
-resposta.innerHTML = valorCod;
+    resposta.innerHTML = valorCod;
 }
 
 
 
-document.getElementById("botaoDecifrar").addEventListener("click",textDecrypt );
+document.getElementById("botaoDecifrar").addEventListener("click", textDecrypt);
 
-function textDecrypt () {
+function textDecrypt() {
 
-let textValue = document.getElementById("cifrarDecifrar").value.toUpperCase();
-let oFfset = document.getElementById("offset").value;
-let converter = Number(oFfset);
-let resposta = document.getElementById("resultEncDec");
-let valorDec = cipher.decode(converter, textValue);
+    let textValue = document.getElementById("cifrarDecifrar").value.toUpperCase();
+    let oFfset = document.getElementById("offset").value;
+    let converter = Number(oFfset);
+    let resposta = document.getElementById("resultEncDec");
+    let valorDec = cipher.decode(converter, textValue);
 
-resposta.innerText = valorDec;
+    resposta.innerText = valorDec;
 }
 
 
